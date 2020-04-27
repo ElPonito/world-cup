5f3e176a078f2e8344385c2c5d3de7b662be7549
Cedric Guerin <guerin.ced@gmail.com>
2020-04-27 12:18:11 +0200

    WIP login

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,22 @@
 import * as express from 'express'
+import * as strava from 'strava-v3'
+import stravaConfig from './conf/stravaConfig'
+
+strava.config(stravaConfig)
 
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000')
+app.get('/token/:code', async (req, res) => {
+    try {
+        const result = await strava.oauth.getToken(req.params.code)
+        res.send(result)
+    } catch (e) {
+        console.error('error occurs on /token request', e)
+        res.status(500).send('Internal Server error')
+    }
+
+})
+
+app.listen(8000, () => {
+    console.log('Server running on port 8000')
 })
---
+++ app/src/conf/index.ts
@@ -0,0 +1,23 @@
+interface Conf {
+    loginUrl: string
+}
+
+interface AppConf {
+    development: Conf
+    production: Conf
+    test: Conf
+}
+
+const conf: AppConf = {
+    development: {
+        loginUrl: 'http://www.strava.com/oauth/authorize?client_id=18363&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read_all,profile:read_all,activity:read_all'
+    },
+    production: {
+        loginUrl: 'TO BE DEFINED'
+    },
+    test: {
+        loginUrl: 'TO BE DEFINED'
+    }
+}
+
+export default conf[process.env.NODE_ENV]
